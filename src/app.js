5a02fb8f948eea35e8ed3ae070f39602bb60130d
ranjith-vara <vararanjith79@gmail.com>
2026-04-10 15:16:17 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 const express = require ('express');
-
 const app = express();
 
-app.use ("/",(req,res)=>{
-    res.send("namasthe Ranjith !");
+app.use("user", (req, res) => {
+    res.send("hahahahahhaha");
 });
 
-app.use ("/hello",(req,res)=>{
-    res.send("hello hello hello!");
+app.post ("/user",(req,res) => {  
+res.send ("data is successfully saved to database!");
 });
 
-app.use ("/test",(req,res)=>{
+app.delete ("/user",(req, res) => {
+    res.send ("data successfully deleted!");
+});
+
+app.get("/user",(req, res) => {
+    res.send({ firstName: "Ranjith", lastName: "vara" });
+})
+ 
+app.use ("/test",(req, res) => {
     res.send("hello from the server!");
 });
 
-app.listen (3000, ()=>{
+app.listen (3000, () => {
     console.log("server is successfully listening on port 3000....")
 });
