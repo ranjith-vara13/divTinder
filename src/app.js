bdfe2784f620c153aa4437e069df99af948da6e1
ranjith-vara <vararanjith79@gmail.com>
2026-04-25 15:23:01 +0530

    data sanitization and user schema

--- src/app.js
+++ src/app.js
@@ -2,21 +2,71 @@ const express = require ('express');
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const user = require('./models/user');
 
 app.use(express.json());
 
-app.post("/signup", async (req, res) => {
+app.post("/signup",async (req, res)=>{
 
-        const user = new User(req.body);
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("user added successfully");
+
+    }catch (err) {
+        res.status (400).send("error saving the user: "+ err.message);
+    } 
+
+});
+
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const user = await User.findOne({emailId: userEmail});
+    res.send(user); 
+} catch (err){
+    res.status(400).send("something went wrong");
+}
+});
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try{ 
+        const user = await User.findByIdAndDelete (userId);
+        res.send ("user deleted successfully");
+    } catch (err) {
+        res.status(400).send("something went wrong");
+    }
+});
+
+
+app.patch("/user", async(req,res) =>{
+    const userId = req.body.userId;
    
-        try {await user.save();
-    res.send("User added successfully!"); 
-} catch (err) {
-    res.status(400).send("Error saving the user: " + err.message);
+    const data = req.body;
+    try {
+        const ALLOWED_UPDATES = ["userId", "photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k)=>
+        ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed){
+        throw new Errow("Update not allowed");
+    }
+    if (data?.skills.length > 10) {
+        throw new Error ("Skills cannot be more than 10");
+    }
 
-}       
-  });
-
+        const user = await User.findByIdAndUpdate({ _id: userId}, data,{ runValidators
+        : true});
+       
+        
+console.log(user);
+        res.send("User updated successfully");
+        } catch (err) {
+            res.status(400).send ("something went wrong");
+        }         
+    });
  connectDB()
  .then(()=>{     console.log("Database connection established..");
     
@@ -28,4 +78,3 @@ app.post("/signup", async (req, res) => {
  .catch((err) =>{
      console.error("Database cannot be connected");
 });
-
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,65 @@
 const mongoose = require ("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
     },
+        
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address: + value");
+            }
+        }
     },
     password: {
         type: String,
+        required: true,
+         validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong Password: "+ value);
+            }
+        },
     },
     age: {
         type: Number,
+        min: 18,
     },
     gender:{
         type:String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)){
+                throw new Error ("Gender data is not valid");
+            }
+        },
+    },
+    photoUrl: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXLb3TY72rHh4VSJUR8UGa83p3ABg3FRBNrw&s", 
+    validate(value){
+            if(!validator.isURL(value)) {
+                throw new Error("Invalid photo URL: " + value);
+            }
+        }
+    },
+    about:{
+        type: String,
+        default: "This is a default about of the user!",
+    },
+    skills:{
+        type: [String],
     },
 });
 
